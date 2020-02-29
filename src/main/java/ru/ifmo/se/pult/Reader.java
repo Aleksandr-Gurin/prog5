59d1602941a5c5b4aafb4b0baebcc26353c76080
Aixoxs <sanyagurin747@gmail.com>
2020-02-29 14:06:04 +0300

    1 commit

--- src/main/java/ru/ifmo/se/pult/Reader.java
+++ src/main/java/ru/ifmo/se/pult/Reader.java
@@ -247,7 +247,6 @@ public class Reader {
                     } catch (NumberFormatException | ArrayIndexOutOfBoundsException | DateTimeException f) {
                         System.out.println("Дата создания введена неверно, повторите попытку");
                         s = in.nextLine();
-
                         dat = s.split("-");
                     }
                 }
